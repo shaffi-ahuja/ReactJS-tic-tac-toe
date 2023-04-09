b00dc7dc708ccecab5fc74b592118301ef75b2e9
shaffi-ahuja <shaffiahuja@Shaffis-MacBook-Air.local>
2023-04-09 14:59:01 +0530

    added winner method

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,31 @@ import './App.css';
 
 
 function Square(props: any) {
-
-  const [value, setValue] = useState("");
-
-
   return <>
     <button className="square" onClick={props.onSquareClick}>{props.value}</button>
   </>
 }
 
+function calculateWinner(squares: Array<any>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] == squares[b] && squares[b] == squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
@@ -20,7 +36,7 @@ export default function Board() {
 
 
   const handleClick = (i: number) => {
-    if (squares[i]) // to prevent overwriting of O with Xon square click and vice versa
+    if (squares[i] || calculateWinner(squares)) // to prevent overwriting of O with Xon square click and vice versa or to return if one player has won
       return;
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? 'X' : 'O';
@@ -28,7 +44,17 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner : " + winner;
+  }
+  else
+    status = "Next player : " + (xIsNext ? "X" : "O")
+
   return <>
+    <div className="status">{status}</div>
+
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
